ad2b0a2363132f926b2c1b8963203eb5e7354783
kushagraj81 <kushagraj81@gmail.com>
2021-01-30 15:52:44 +0530

    corrected end to env in listen

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(process.end.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Serving on port 3000')
 })
 
